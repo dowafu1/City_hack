579dd7dd5762237e8921de23fc9b2abedf827135
realReloadTime <max.shilcko@yandex.ru>
2025-09-04 14:59:43 +0700

    MistralAI is available for use

--- ai/mistral_ai.py
+++ ai/mistral_ai.py
@@ -1,22 +1,39 @@
 import os
+import asyncio
+
 from dotenv import load_dotenv
 from mistralai import Mistral
+from mistralai.models import UserMessage, SystemMessage, ChatCompletionResponse
 
 load_dotenv()
 
-api_key = os.getenv('MISTRAL_API_KEY')
+api_key = os.getenv('MISTRAL_TOKEN')
 model = "mistral-large-latest"
-
 client = Mistral(api_key=api_key)
 
-chat_response = client.chat.complete(
-    model = model,
-    messages = [
-        {
-            "role": "user",
-            "content": "What is the best French cheese?",
-        },
-    ]
-)
+sessions = {}
 
-print(chat_response.choices[0].message.content)
+
+async def make_chat(prompt: str, user_id: int) -> ChatCompletionResponse:
+    if user_id not in sessions:
+        history = list()
+        history.append(SystemMessage(
+            content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы. Отвечай только на русском языке."))
+    else:
+        history = sessions[user_id]
+
+    history.append(UserMessage(content=prompt))
+
+    response = await client.chat.complete_async(
+        model=model,
+        messages=history,
+    )
+    return response.choices[0].message.content
+
+
+async def main():
+    print(await make_chat(prompt="What do you want to do? I'm russian.", user_id=1))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
