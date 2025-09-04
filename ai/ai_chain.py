f038e06afb5ca73e6ceb6a7339bd6419993fd4f9
realReloadTime <max.shilcko@yandex.ru>
2025-09-04 17:13:33 +0700

    Context from files /context now loading to neural prompt context

--- ai/ai_chain.py
+++ ai/ai_chain.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from sber_ai import make_chat as sber_chat
 from mistral_ai import make_chat as mistral_chat
@@ -7,9 +8,14 @@ from mistralai import Mistral
 from mistralai.models.sdkerror import SDKError
 from langchain_gigachat.chat_models import GigaChat
 
+from aiofiles import open as aio_open
+
 
 async def chainize(user_prompt: str, history: list, sber: GigaChat, mistral: Mistral) -> str | None:
-    tries_count = 5
+    tries_count = 7
+    context_data = await get_context_data('context')
+    if context_data is not None:
+        context_data = [f'Файл "{key}", содержание: {value}' for key, value in context_data.items()]
     try:
         total_answer = await sber_chat(sber, user_prompt, history)
     except Exception as e:
@@ -25,16 +31,22 @@ async def chainize(user_prompt: str, history: list, sber: GigaChat, mistral: Mis
                               'Проанализируй ответ коллеги, и предложи улучшенный ответ клиенту, '
                               'если есть что улучшать, иначе оставь все как есть. '
                               'В качестве ответа напиши только текст-ответ клиенту, как если бы ты писал ему в личные сообщения Telegram. '
-                              'Можешь добавить визуальные элементы (emoji), если считаешь это уместным.')
+                              f'Можешь добавить визуальные элементы (emoji), если считаешь это уместным. '
+                              f'Еще у тебя есть теория, которая тебе может помочь разобраться с проблемой: {'\n'.join(context_data)}')
+
             total_answer = mistral_answer
+
             break
+
         except SDKError:
             print(f'Mistral недоступен, попытка {i + 1} из {tries_count}')
             await asyncio.sleep(1)
+
             continue
 
         except Exception as e:
             print(e)
+
             return None
 
     return total_answer
@@ -48,13 +60,21 @@ async def get_tip(sber: GigaChat, prev_tips: list[str]) -> str | None:
             [],
             preset_prompt=f'Ты эмпатичный психолог-профессионал, ведущий телеграмм-канал. '
                           f'Каждый день ты пишешь совет дня для своих подписчиков. Напиши его, не бойся быть креативным. '
-                          f'Твои предыдущие советы (разделены &): {"&".join(prev_tips)}.'
+                          f'Твои предыдущие советы (разделены &): {"&".join(prev_tips) if len(prev_tips) else "советов еще не было"}.'
         )
         return total_answer
 
     except Exception as e:
         print(f'Обвал SberAI в ai/ai_chain.py get_tip: {e}')
     return None
+
+
+async def get_context_data(context_directory: str = 'context') -> dict[str, str] | None:
+    texts = {}
+    for file in [file for file in os.walk('context')][0][2:][0]:
+        async with aio_open(f'{context_directory}/{file}', 'r', encoding='utf-8') as f:
+            texts[file] = await f.read()
+    return texts if texts else None
 
 
 async def main():
@@ -65,7 +85,7 @@ async def main():
     sber_client = GigaChat(credentials=os.getenv('SBER_TOKEN'), verify_ssl_certs=False)
     mistral_client = Mistral(api_key=os.getenv('MISTRAL_TOKEN'))
 
-    print(await chainize('Привет, я чувствую себя ужасно. Что мне делать?',
+    print(await chainize('Я не могу есть и смотреть на еду. Что мне делать?',
                          [], sber_client, mistral_client
                          ))
     print(await get_tip(sber_client,
